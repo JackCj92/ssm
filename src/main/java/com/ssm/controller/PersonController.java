c35872fc72ff547d8e4cc8c1feb7527b8616eb99
JackCj92 <jackcj2020@163.com>
2020-12-17 18:52:20 +0800

    Java-ssm框架

--- src/main/java/com/ssm/controller/PersonController.java
+++ src/main/java/com/ssm/controller/PersonController.java
@@ -2,7 +2,6 @@ package com.ssm.controller;
 
 import com.ssm.entity.Person;
 import com.ssm.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +11,16 @@ import java.util.List;
 @Controller
 public class PersonController {
 
-    @Autowired
-    private PersonService personService;
+    private final PersonService personService;
+    //基于构造函数的依赖注入
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
 
     @RequestMapping("/showPerson")
     public String showPersons(Model model) {
         List<Person> persons = personService.findAll();
         model.addAttribute("persons", persons);
-        return "jsp/showperson";
+        return "jsp/showPerson";
     }
 }
--- src/main/java/com/ssm/service/impl/PersonServiceImpl.java
+++ src/main/java/com/ssm/service/impl/PersonServiceImpl.java
@@ -3,18 +3,22 @@ package com.ssm.service.impl;
 import com.ssm.dao.PersonDao;
 import com.ssm.entity.Person;
 import com.ssm.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service("PersonService")
-@Transactional
+@Transactional(isolation = Isolation.READ_COMMITTED,propagation = Propagation.REQUIRED,timeout = 1000)
 public class PersonServiceImpl implements PersonService {
 
-    @Autowired
-    private PersonDao personDao;
+    private final PersonDao personDao;
+    //基于构造函数的依赖注入
+    public PersonServiceImpl(PersonDao personDao) {
+        this.personDao = personDao;
+    }
 
     @Override
     public List<Person> findAll() {
